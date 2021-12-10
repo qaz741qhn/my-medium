0d654881bcdc1fb9df85783ee6a68e37c00cede4
qaz741qhn <qaz741qhn@gmail.com>
2021-12-10 19:04:01 +0900

    fix pages#show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,21 @@
 class PagesController < ApplicationController
 
+  before_action :set_story, only: [:show]
+
   def index
-    @stories = Story.published.order(created_at: :desc).includes(:user)
+    @stories = Story.published_stories
   end
 
   def show
   end
 
   def user
+  end
+
+  private
+
+  def set_story
+    @story = Story.friendly.find(params[:story_id])
   end
 
 end
