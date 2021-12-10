5cbbf19321778c3acbfd903e85ede254c9634b56
qaz741qhn <qaz741qhn@gmail.com>
2021-12-10 17:09:30 +0900

    add stimulus & foreman

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get '@:username/:story_id', to: 'pages#show', as: 'story_page'
   get '@:username', to: 'pages#user', as: 'user_page'
 
+  get '/demo', to: 'pages#demo'
   root 'pages#index'
 end
